2603dd2924fd37f73c6e571b699b4706f009c4f7
Patrick Munoz <patrick.munoz@ombori.com>
2019-10-23 23:33:25 +0800

    add configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import Provider from "oidc-provider";
+import { ClientMetadata, Configuration, Provider } from "oidc-provider";
 
 mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -14,11 +14,25 @@ db.once("open", () => {
 const app = express();
 const appPort = 3000;
 
+const client: ClientMetadata = {
+  client_id: "foo",
+  client_secret: "bar",
+  redirect_uris: ["http://lvh.me:8080/cb"]
+};
+
+const configuration: Configuration = {
+  // ... see available options /docs
+  clients: [
+    client,
+    // + other client properties
+  ],
+};
+
 app.set("port", appPort);
 
 app.get("/", (req, res) => {
   res.send("The sedulous hyena ate the antelope!");
 });
-const oidc = new Provider("http://localhost:3000");
-app.use("/", oidc.callback);
+const provider = new Provider("http://localhost:3000", configuration);
+app.use("/", provider.callback);
 app.listen(app.get("port"));
