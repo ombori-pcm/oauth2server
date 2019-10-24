7187107cd83904fa9647f4d5fa8a9717a9b38014
Patrick Munoz <patrick.munoz@ombori.com>
2019-10-24 20:44:05 +0800

    features and scopes

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,42 @@ db.once("open", () => {
 const app = express();
 const appPort = 3000;
 
-const client: ClientMetadata = {
-  client_id: "foo",
+const client1: ClientMetadata = {
+  client_id: "test_implicit_app",
   client_secret: "bar",
-  redirect_uris: ["http://lvh.me:8080/cb"]
+  grant_types: ["implicit"],
+  redirect_uris: ["https://testapp/signin-oidc"],
+  response_types: ["id_token"],
+  token_endpoint_auth_method: "none",
 };
+
+const client2: ClientMetadata = {
+  client_id: "test_oauth_app",
+  client_secret: "super_secret",
+  grant_types: ["client_credentials"],
+  redirect_uris: [],
+  response_types: [],
+}
 
 const configuration: Configuration = {
   // ... see available options /docs
   clients: [
-    client,
+    client1, client2,
     // + other client properties
   ],
+  features: {
+    clientCredentials: { enabled: true },
+    devInteractions: { enabled: true },
+    introspection: { enabled: true },
+  },
+  scopes: ["api"],
 };
 
 app.set("port", appPort);
 
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
-});
+// app.get("/", (req, res) => {
+//   res.send("The sedulous hyena ate the antelope!");
+// });
 const provider = new Provider("http://localhost:3000", configuration);
 app.use("/", provider.callback);
 app.listen(app.get("port"));
